a6537dcb899c8ce2d506052dd074c6c3d8c24934
Zainab <202201685@student.polytechnic.bh>
2024-12-22 08:18:21 +0300

    Ddded profile screen

--- Happy Journey/HappyJourney/Profile.Designer.cs
+++ Happy Journey/HappyJourney/Profile.Designer.cs
@@ -28,20 +28,165 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnSignOut = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtRole = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtNationality = new System.Windows.Forms.TextBox();
+            this.txtDob = new System.Windows.Forms.TextBox();
+            this.labelName = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.mnuProfile = new System.Windows.Forms.MenuStrip();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // btnSignOut
+            // 
+            this.btnSignOut.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnSignOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSignOut.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSignOut.Location = new System.Drawing.Point(483, 606);
+            this.btnSignOut.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSignOut.Name = "btnSignOut";
+            this.btnSignOut.Size = new System.Drawing.Size(487, 50);
+            this.btnSignOut.TabIndex = 18;
+            this.btnSignOut.Text = "Sign Out";
+            this.btnSignOut.UseVisualStyleBackColor = false;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEdit.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnEdit.Location = new System.Drawing.Point(483, 534);
+            this.btnEdit.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(487, 50);
+            this.btnEdit.TabIndex = 17;
+            this.btnEdit.Text = "Edit Profile";
+            this.btnEdit.UseVisualStyleBackColor = false;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtEmail.Location = new System.Drawing.Point(483, 479);
+            this.txtEmail.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.ReadOnly = true;
+            this.txtEmail.Size = new System.Drawing.Size(487, 31);
+            this.txtEmail.TabIndex = 16;
+            // 
+            // txtRole
+            // 
+            this.txtRole.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtRole.Location = new System.Drawing.Point(737, 429);
+            this.txtRole.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtRole.Name = "txtRole";
+            this.txtRole.ReadOnly = true;
+            this.txtRole.Size = new System.Drawing.Size(233, 31);
+            this.txtRole.TabIndex = 15;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPhone.Location = new System.Drawing.Point(483, 429);
+            this.txtPhone.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.ReadOnly = true;
+            this.txtPhone.Size = new System.Drawing.Size(240, 31);
+            this.txtPhone.TabIndex = 14;
+            // 
+            // txtNationality
+            // 
+            this.txtNationality.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtNationality.Location = new System.Drawing.Point(737, 379);
+            this.txtNationality.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtNationality.Name = "txtNationality";
+            this.txtNationality.ReadOnly = true;
+            this.txtNationality.Size = new System.Drawing.Size(233, 31);
+            this.txtNationality.TabIndex = 13;
+            // 
+            // txtDob
+            // 
+            this.txtDob.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtDob.Location = new System.Drawing.Point(483, 379);
+            this.txtDob.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtDob.Name = "txtDob";
+            this.txtDob.ReadOnly = true;
+            this.txtDob.Size = new System.Drawing.Size(240, 31);
+            this.txtDob.TabIndex = 12;
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelName.Location = new System.Drawing.Point(520, 305);
+            this.labelName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelName.MinimumSize = new System.Drawing.Size(450, 0);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(450, 37);
+            this.labelName.TabIndex = 11;
+            this.labelName.Text = "Name Name Name";
+            this.labelName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::HappyJourney.Properties.Resources.Profile_Pic;
+            this.pictureBox1.Location = new System.Drawing.Point(633, 75);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 23;
+            this.pictureBox1.TabStop = false;
+            // 
+            // mnuProfile
+            // 
+            this.mnuProfile.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.mnuProfile.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.mnuProfile.Location = new System.Drawing.Point(0, 0);
+            this.mnuProfile.Name = "mnuProfile";
+            this.mnuProfile.Size = new System.Drawing.Size(1472, 42);
+            this.mnuProfile.TabIndex = 24;
+            this.mnuProfile.Text = "menuStrip1";
             // 
             // Profile
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1472, 719);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnSignOut);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtRole);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtNationality);
+            this.Controls.Add(this.txtDob);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.mnuProfile);
+            this.MainMenuStrip = this.mnuProfile;
             this.Name = "Profile";
             this.Text = "Profile";
             this.Load += new System.EventHandler(this.Profile_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSignOut;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtRole;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtNationality;
+        private System.Windows.Forms.TextBox txtDob;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip mnuProfile;
     }
 }
--- Happy Journey/HappyJourney/Profile.cs
+++ Happy Journey/HappyJourney/Profile.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using HappyJourney.entities;
+using HappyJourney.observers;
+using HappyJourney.singletons;
 
 namespace HappyJourney
 {
@@ -14,18 +19,161 @@ namespace HappyJourney
     {
         private int loggedInUserId;
         private int loggedInUserRoleId;
-        private int passedUserId;
 
         public Profile(int userId, int roleId)
         {
             InitializeComponent();
             loggedInUserId = userId;
             loggedInUserRoleId = roleId;
+            SetupMenuStrip();
+
+            User user = UserSession.Instance.CurrentUser;
+
+            if (user != null)
+            {
+                labelName.Text = user.FullName;
+                txtDob.Text = user.DateOfBirth;
+                txtEmail.Text = user.Email;
+                txtPhone.Text = user.Phone;
+                txtNationality.Text = user.Nationality;
+
+                string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+                // Query for flight details
+                string query = @"
+            SELECT role_name
+            FROM UserRole
+            WHERE role_id = @RoleId";
+
+                try
+                {
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        connection.Open();
+
+                        // Load flight details
+                        using (SqlCommand cmd = new SqlCommand(query, connection))
+                        {
+                            cmd.Parameters.AddWithValue("@RoleId", user.RoleId);
+                            txtRole.Text = cmd.ExecuteScalar().ToString();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error loading role name: {ex.Message}", "Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+            UserObserver.Subscribe(() =>
+            {
+                User newUser = UserSession.Instance.CurrentUser;
+
+                if (newUser != null)
+                {
+                    labelName.Text = newUser.FullName;
+                    txtPhone.Text = newUser.Phone;
+                }
+            });
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            EditProfile editProfile = new EditProfile();
+            editProfile.ShowDialog();
+        }
+
+        private void btnSignOut_Click(object sender, EventArgs e)
+        {
+            UserSession.Instance.ClearSession();
+
+            Signup signup = new Signup();
+            signup.Show();
+            Hide();
         }
 
         private void Profile_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void SetupMenuStrip()
+        {
+            MenuStrip menuStrip = new MenuStrip();
+
+            ToolStripMenuItem homeItem = new ToolStripMenuItem("Home");
+            homeItem.Click += (s, e) =>
+            {
+                MessageBox.Show("You are already on the Home page.");
+            };
+
+            ToolStripMenuItem profileItem = new ToolStripMenuItem("Profile");
+            profileItem.Click += (s, e) => NavigateToProfile();
+
+            ToolStripMenuItem inboxItem = new ToolStripMenuItem("Inbox");
+            inboxItem.Click += (s, e) => NavigateToInbox();
+
+            ToolStripMenuItem composeItem = new ToolStripMenuItem("Compose");
+            composeItem.Click += (s, e) => NavigateToCompose();
+
+            menuStrip.Items.Add(homeItem);
+
+            if (loggedInUserRoleId == 1)
+            {
+                ToolStripMenuItem dashboardItem = new ToolStripMenuItem("Dashboard");
+                dashboardItem.Click += (s, e) => NavigateToDashboard();
+                menuStrip.Items.Add(dashboardItem);
+            }
+
+            else if (loggedInUserRoleId == 3)
+            {
+                ToolStripMenuItem bookingsItem = new ToolStripMenuItem("Bookings");
+                bookingsItem.Click += (s, e) => NavigateToBookings();
+                menuStrip.Items.Add(bookingsItem);
+            }
+
+            menuStrip.Items.Add(profileItem);
+            menuStrip.Items.Add(inboxItem);
+            menuStrip.Items.Add(composeItem);
+
+            this.mnuProfile = menuStrip;
+            this.Controls.Add(menuStrip);
+        }
+
+        private void NavigateToInbox()
+        {
+            Inbox inbox = new Inbox(loggedInUserId, loggedInUserRoleId);
+            inbox.ShowDialog();
+        }
+
+        private void NavigateToCompose()
+        {
+            Compose compose = new Compose(loggedInUserId, loggedInUserRoleId);
+            compose.ShowDialog();
+        }
+
+        private void NavigateToProfile()
+        {
+            Profile profile = new Profile(loggedInUserId, loggedInUserRoleId);
+            profile.Show();
+        }
+
+        private void NavigateToDashboard()
+        {
+            Dashboard dashboard = new Dashboard(loggedInUserId, loggedInUserRoleId);
+            dashboard.ShowDialog();
+        }
+
+        private void NavigateToBookings()
+        {
+            Bookings bookings = new Bookings(loggedInUserId, loggedInUserRoleId);
+            bookings.ShowDialog();
+        }
+
+        private void NavigateToHome()
+        {
+            MessageBox.Show("You are already on the Home page.");
         }
     }
 }
